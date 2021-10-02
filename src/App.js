23be8c7d0bfefaa7cbc282f761685c26a3847fc2
Jibonm37 <jibonrahman399@gmail.com>
2021-10-02 23:57:25 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/inventory/Inventory';
+import OrderReview from './components/OrderReview/OrderReview';
+import PlaceOrder from './components/Placeorder/PlaceOrder';
 import Shop from './components/Shop/Shop';
+import NotFund from './notFund/NotFund';
 
 function App() {
   return (
     <div>
+      
+      <BrowserRouter>
       <Header></Header>
+      <Switch>
+      <Route exact path='/'>
       <Shop></Shop>
+      </Route>
+      <Route path='/shop'>
+      <Shop></Shop>
+      </Route>
+      <Route path='/review'>
+        <OrderReview></OrderReview>
+      </Route>
+      <Route path='/inventory'>
+        <Inventory>
+
+        </Inventory>
+      </Route>
+      <Route path='/placeorder'>
+      <PlaceOrder></PlaceOrder>
+      </Route>
+      <Route path='*'>
+        <NotFund>
+        </NotFund>
+      </Route>
+      </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/notFund/NotFund.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFund = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h3>Page is not available</h3>
+        </div>
+    );
+};
+
+export default NotFund;
